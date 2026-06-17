202c265d3b232b365222a66e57e9099cb6ef84a8
PedroHamamoto <hamamoto.pedro@gmail.com>
2026-06-16 19:04:30 -0700

    Adding the r2 client

---
+++ server/src/infrastructure/storage/upload-file-to-storage.ts
@@ -0,0 +1,39 @@
+import { randomUUID } from 'node:crypto'
+import { Readable } from 'node:stream'
+import { Upload } from '@aws-sdk/lib-storage'
+import z from 'zod'
+import { env } from '@/infrastructure/env'
+import { r2 } from './client'
+
+const uploadFileToStorageInput = z.object({
+    contentType: z.string(),
+    contentStream: z.instanceof(Readable),
+})
+
+type UploadFileToStorageInput = z.input<typeof uploadFileToStorageInput>
+
+export async function uploadFileToStorage(input: UploadFileToStorageInput) {
+    const { contentType, contentStream } =
+        uploadFileToStorageInput.parse(input)
+
+    const now = new Date()
+    const dateFormat = now.toISOString().slice(0, 10)
+    const uniqueFileName = `exports/${dateFormat}-${randomUUID()}-links.csv`
+
+    const upload = new Upload({
+        client: r2,
+        params: {
+            Bucket: env.CLOUDFLARE_R2_BUCKET_NAME,
+            Key: uniqueFileName,
+            Body: contentStream,
+            ContentType: contentType,
+        },
+    })
+
+    await upload.done()
+
+    return {
+        key: uniqueFileName,
+        url: new URL(uniqueFileName, env.CLOUDFLARE_BUCKET_PUBLIC_URL).toString(),
+    }
+}
